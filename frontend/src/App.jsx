96690472afd82b69f192244863e16e549f352742
BakhatMuneerDev <bakhatmuneer.webdev@gmail.com>
2025-03-07 16:14:54 +0500

    add lucide-react for loading spinner and update routing logic based on authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,9 @@
-import Navbar from "./components/Navbar"
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-
+// Import native components
+import { Loader } from "lucide-react";
+// Import Manual Components
+import Navbar from "./components/Navbar"
 // authStore
 import { useAuthStore } from "./store/useAuthStore";
 // Importing pages from page Folder
@@ -9,28 +12,33 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ProfilePage from "./pages/ProfilePage";
 import SettingsPage from "./pages/SettingsPage";
-import { useEffect } from "react";
 
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore()
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
   // call imediatly as our application loading
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
-  console.log({ authUser })
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    )
+  }
   return (
     <div>
 
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser? <SignUpPage />: <Navigate to="/"/>} />
+        <Route path="/login" element={!authUser? <LoginPage />: <Navigate to="/"/>} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser? <ProfilePage />:<Navigate to="/login"/>} />
       </Routes>
     </div>
   )
